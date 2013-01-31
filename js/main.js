70abc5a7ef1ec42b26de81eba93798c1d17a0a9d
Keith Gagne <keith_gagne@hotmail.com>
2013-01-31 08:36:54 -0500

    Update

--- js/main.js
+++ js/main.js
@@ -1,16 +1,79 @@
+var dom = {
+	header 	: $('.header'),
+	content	: $('.content'),
+	footer	: $('.footer')
+}
 var app = {
-		header 		: {
-			width		: $('.header').width(),
-			height		: $('.header').height()
-		},
-		footer 		: {
-			width		: $('.footer').width(),
-			height		: $('.footer').height()
-		},
-		viewport	: function(elem){
-			elem.css({ 'height' : $(window).height() - (app.header.height + app.footer.height) + 'px' })
-		}
+	init : function(){
+		
+		//Set the size of the content viewport
+		dom.content.css({ 'height' : ($(window).height() - (dom.header.height() + dom.footer.height())) + 'px' })
+		
+		//Initialize the navigation width
+		app.sizeNav()
+		//Initialize the content boxes
+		app.sizeContent()
+		//Bind events to navigation clicks
+		app.bindNav()	
+		
+	},
+	sizeNav : function(){
+		
+		//Size the navigation buttons based on amount and window width
+		$('a').each(function(key, value){
+			
+			console.log(key + ' <=> ' + ($('.footer a').length - 1))
+				
+			$(this).attr('data-key', key)
+			
+			if(key < ($('.footer a').length - 1)){
+				
+				$(this).css({ 'border-right' : '1px solid rgba(0,0,0,0.2)' })
+				
+			}
+			
+		})
+		
+		$('.footer a').css({ 'width' : ((dom.footer.width() / $('.footer a').length) - 1) + 'px' })
+		
+	},
+	sizeContent : function(){
+		
+		$('.content').html('<div class="pager">'+ $('.content').html() +'</div>')
+		
+		$('.content .pager').css({ 'width' : $('.footer a').length + '00%' })
+
+		$('.content .pager .page').css({ 'width' : dom.content.width() + 'px' })
+		
+	},
+	bindNav : function(){
+		
+		$('.footer a').on('click', function(){
+			
+			//Add transition for button animation
+			$('.footer a').css({ '-webkit-transition' : 'all .1s' })
+			$(this).find('div').css({ '-webkit-transition' : 'all .2s' })
+			
+			if($(this).hasClass('active')) return false
+			
+			$('.footer a').removeClass('active')
+			$(this).find('div').css({ 'opacity' : '0' })
+			
+			$(this).addClass('active')
+			$(this).find('div').css({ 'opacity' : '1' })
+			
+			$('.content .pager').css({ '-webkit-transition' : 'all .3s ease-out', '-webkit-transform' : 'translate3d(-'+ ((100 / $('.footer a').length) * $(this).attr('data-key')) +'%,0,0)' })
+			
+			$('.header h1').text($(this).attr('data-title'))
+			
+			return false
+			
+		})
+		
 	}
+}
 $(function(){
-	app.viewport($('.content'))
+	
+	app.init()
+	
 })
